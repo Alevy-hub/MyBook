afa87c516bc661d60f11404120970a60d32693b4
Rafal Kujawa <rkujawa@edu.cdv.pl>
2022-07-19 21:41:42 +0200

    Dodaj pliki projektów.

---
+++ MainScreen.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace MyBook
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.TopBarPanel = new System.Windows.Forms.Panel();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(350, 900);
+            this.MenuPanel.TabIndex = 0;
+            // 
+            // TopBarPanel
+            // 
+            this.TopBarPanel.Location = new System.Drawing.Point(350, 0);
+            this.TopBarPanel.Name = "TopBarPanel";
+            this.TopBarPanel.Size = new System.Drawing.Size(1090, 100);
+            this.TopBarPanel.TabIndex = 1;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Location = new System.Drawing.Point(350, 100);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1090, 800);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1440, 900);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.TopBarPanel);
+            this.Controls.Add(this.MenuPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainScreen";
+            this.Text = "MainScreen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel TopBarPanel;
+        private System.Windows.Forms.Panel MainPanel;
+    }
+}
